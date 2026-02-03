77c1ae2441c843c3991b3744ba36336d504c89cf
Reyz4You Official <raihanananda767@gmail.com>
2026-02-03 21:49:36 +0700

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,6 @@
+// api/config.js
+module.exports = {
+    MONGODB_URI: "mongodb+srv://Reyz4You:Reyz4YouDev@cluster0.fuqnnfe.mongodb.net/?appName=Cluster0",
+    PAKASIR_API_KEY: "URaa2XAwEHwXrO0LTZcFUfLBozUQqdON",
+    SLUG_PAKASIR: "reyz4youshop"
+};
